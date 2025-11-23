12ebf5667c45d5b6759bc3f5154538b8105fe023
AngelD89 <angeldeprey@yahoo.com>
2025-11-23 13:54:15 -0500

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-"""
-Module 0-add
+"""Type-annotated add function module."""
 
-This is module that provides function 'add' and returns the sum of two float numbers.
-"""
 
 def add(a: float, b: float) -> float:
-    """Return the sum of two floats."""
+    """Adds two floats."""
     return a + b
