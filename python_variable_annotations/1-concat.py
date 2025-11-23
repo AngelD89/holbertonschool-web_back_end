a4f6511d9df40f61077554e8d9d3c1ed58caa851
AngelD89 <angeldeprey@yahoo.com>
2025-11-23 13:57:55 -0500

    1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-concat = __import__('1-concat').concat
+"""Type-annotated concat function module."""
 
-str1 = "egg"
-str2 = "shell"
 
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
+def concat(str1, str2: str) -> str:
+    """Concatenates two strings."""
+    return str1 + str2
