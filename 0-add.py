817dc0911d0f4577ffeaacd7468a9e21f9d273c9
AngelD89 <angeldeprey@yahoo.com>
2025-11-23 13:10:53 -0500

    0-add.py

---
+++ 0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module for a simple addition function with type annotations."""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats."""
+    return a + b
