f066ddab7234f1ef3018c9c53f078789e5163f35
AngelD89 <angeldeprey@yahoo.com>
2025-11-23 14:43:08 -0500

    1-concurrent_coroutines.py

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Asynchronous wait_n coroutine module."""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times and returns list of delays in ascending order."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
