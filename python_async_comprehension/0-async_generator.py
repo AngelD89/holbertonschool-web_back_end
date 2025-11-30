44bd0d728525f72e0f9158d93416165297d28dd2
AngelD89 <angeldeprey@yahoo.com>
2025-11-30 15:43:50 -0400

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """0-async_generator.py
-Async generator that yields 10 random integers between 0 and 10,
+Asynchronous generator that yields 10 random integers between 0 and 10,
 waiting 1 second between each yield.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
 
-async def async_generator() -> AsyncGenerator[int, None]:
-    """Asynchronous generator that yields 10 random integers (0..10)."""
+
+async def async_generator():
+    """Async generator that yields 10 random integers (0..10)."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
