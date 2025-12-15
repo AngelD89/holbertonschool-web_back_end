23f1dcdf1f2c415d8f3d9b119ce90c04ce53b821
AngelD89 <angeldeprey@yahoo.com>
2025-12-14 22:17:32 -0400

    10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,28 +1,23 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   get brand() {
-    return this[_brand];
+    return this._brand;
   }
 
   get motor() {
-    return this[_motor];
+    return this._motor;
   }
 
   get color() {
-    return this[_color];
+    return this._color;
   }
 
   cloneCar() {
-    const CloneClass = this.constructor[Symbol.species] || this.constructor;
-    return new CloneClass();
+    return new this.constructor();
   }
 }
