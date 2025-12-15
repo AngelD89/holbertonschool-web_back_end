dbd9bf77055df547a8b3eeed3e25d9f17f7867b1
AngelD89 <angeldeprey@yahoo.com>
2025-12-14 21:37:22 -0400

    0-classroom.js

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-  constructor(maxstudentsSize) {
-    this._maxstudentsSize = maxstudentsSize;
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
