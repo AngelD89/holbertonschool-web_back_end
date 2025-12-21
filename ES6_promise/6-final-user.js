89fa5dffc194a431d19533cf2963540fbf5db167
AngelD89 <angeldeprey@yahoo.com>
2025-12-21 12:09:29 -0400

    6-final

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,15 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+  const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => {
-    return results.map((result) => {
-      const obj = {};
-      obj.status = result.status;
-      obj.value = result.reason !== undefined ? result.reason : result.value;
-      return obj;
-    });
+  ]);
+
+  return results.map((res) => {
+    if (res.status === 'fulfilled') {
+      return { status: res.status, value: res.value };
+    }
+    return { status: res.status, value: res.reason.toString() };
   });
 }
