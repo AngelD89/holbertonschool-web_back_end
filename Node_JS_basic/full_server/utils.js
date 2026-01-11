cfa100809f8f028e7326b0b53a9b9b1df7141405
AngelD89 <angeldeprey@yahoo.com>
2026-01-11 15:05:59 -0400

    utils.js

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+export function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const lines = data.split('\n');
+
+      // Remove header and filter out empty lines
+      const students = lines
+        .slice(1)
+        .filter((line) => line.trim().length > 0)
+        .map((line) => line.split(','));
+
+      // Group students by field
+      const fields = {};
+      students.forEach((student) => {
+        const field = student[3];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(student[0]);
+      });
+
+      resolve(fields);
+    });
+  });
+}
